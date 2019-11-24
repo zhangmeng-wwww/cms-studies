9bd748644264224079988eae217cf4545a9f7454
zhangmeng <895080927@qq.com>
2019-11-24 19:24:46 +0800

    '初始化项目'

---
+++ src/main/java/com/briup/cms/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,19 @@
+package com.briup.cms.service.impl;
+
+import com.briup.cms.bean.Customer;
+import com.briup.cms.dao.ICustomerDao;
+import com.briup.cms.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements ICustomerService {
+
+    @Autowired
+    private ICustomerDao customerDao;
+
+    @Override
+    public Customer fineById(int id) {
+        return customerDao.queryById(id);
+    }
+}
